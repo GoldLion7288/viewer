42c82626936c420ee8077c46289887632fec08dc
GoldLion7288 <candoeverything728@gmail.com>
2025-11-01 04:48:25 +0900

    end

--- hand.sh
+++ hand.sh
@@ -1,7 +1,33 @@
-~/viewer/run.sh start ~/data/background.jpg 1
-~/viewer/run.sh play ~/data/test1.mp4 5
-~/viewer/run.sh play ~/data/test2.mp4 8
-~/viewer/run.sh play ~/data/test3.mp4 10
-~/viewer/run.sh play ~/data/test4.mp4 10
-~/viewer/run.sh stop
-~/viewer/run.sh exit
+#!/bin/bash
+
+# Setup environment for X display (required for GUI)
+export DISPLAY=:0
+export XAUTHORITY=/home/pi/.Xauthority
+
+# Wait for X server to be ready
+sleep 5
+
+# Use absolute paths (~ doesn't work in cron)
+VIEWER="/home/pi/viewer/run.sh"
+DATA="/home/pi/data"
+
+# Log start time
+echo "========================================" >> /home/pi/hand.log
+echo "Starting at: $(date)" >> /home/pi/hand.log
+
+# Check if viewer script exists
+if [ ! -f "$VIEWER" ]; then
+    echo "ERROR: $VIEWER not found!" >> /home/pi/hand.log
+    exit 1
+fi
+
+# Run the playlist
+"$VIEWER" start "$DATA/background.jpg" 1
+"$VIEWER" play "$DATA/test1.mp4" 5
+"$VIEWER" play "$DATA/test2.mp4" 8
+"$VIEWER" play "$DATA/test3.mp4" 10
+"$VIEWER" play "$DATA/test4.mp4" 10
+"$VIEWER" stop
+"$VIEWER" exit
+
+echo "Finished at: $(date)" >> /home/pi/hand.log
