8c0cc6203b768bfb4a966398124dd0ea47c1ef8a
GoldLion7288 <candoeverything728@gmail.com>
2025-11-02 01:29:39 +0900

    fix: asyn video and audio(gst) +21

--- testrun.sh
+++ testrun.sh
@@ -1,7 +1,7 @@
 ~/viewer/run.sh start ~/data/background.jpg 1
 ~/viewer/run.sh play ~/data/test1.mp4 30
+~/viewer/run.sh play ~/data/test4.mp4 50
 ~/viewer/run.sh play ~/data/test2.mp4 5
 ~/viewer/run.sh play ~/data/test3.mp4 5
-~/viewer/run.sh play ~/data/test4.mp4 150
 ~/viewer/run.sh stop
 ~/viewer/run.sh exit
